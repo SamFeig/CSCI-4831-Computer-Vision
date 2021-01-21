f81795c132ca077686f8aaf37f15cf76ead3b3de
Sam Feig <spfeig@gmail.com>
2021-01-21 14:35:16 -0700

    Binary Mask

--- H1/binaryMask.m
+++ H1/binaryMask.m
@@ -1,11 +1,25 @@
 function [ outImg] = binaryMask(inImg)
-
-
-
-
-
-    %binaryImage = inImg > 100; %inImg > 80 & inImg < 160;
-    %outImg = ~bwareafilt(binaryImage, 1);
+    [x, y]=size(inImg, [1,2]); 
+    inImg = double(inImg); 
     
+    sum = 0; 
+    for i=1:x 
+        for j=1:y 
+            sum = sum + inImg(i, j); 
+        end
+    end
+     
+    threshold = sum / ( x * y ); 
+    outImg = zeros(x, y);
+    
+    for i=1:x 
+        for j=1:y 
+            if inImg(i, j) >= threshold 
+                outImg(i, j) = 0; 
+            else
+                outImg(i, j)=1; 
+            end
+        end
+    end   
 end
 
