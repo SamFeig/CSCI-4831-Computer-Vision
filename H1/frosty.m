77fecc3660cb32b11172dd429ad2d465fb804593
Sam Feig <spfeig@gmail.com>
2021-01-22 13:48:15 -0700

    Minor naming changes

--- H1/frosty.m
+++ H1/frosty.m
@@ -15,33 +15,5 @@ function [ outImg ] = frosty( inImg, n, m )
             outImg(i, j, :) = inImg(randi([m_left m_right]), randi([n_left n_right]), :);
         end
     end
-%     for i = 1:x
-%         for j = 1:y
-%                 neighbors = {};
-%                 for nn = -n/2:n/2
-%                     for mm = -m/2:m/2
-%                         idxx = i+nn;
-%                         idxy = j+mm;
-%                         if idxx < 1
-%                             idxx = 1;
-%                         elseif idxx > x
-%                             idxx = x;
-%                         end
-%                         if idxy < 1
-%                             idxy = 1;
-%                         elseif idxy > y
-%                             idxy = y;
-%                         end
-%                         % disp(inImg(idxx, idxy, :))
-%                         neighbors = [neighbors, inImg(idxx, idxy, :)];
-%                     end
-%                 end
-% 
-%                 frost = randi(numel(neighbors), 1);
-%                 % disp(neighbors{frost})
-% 
-%                 outImg(i, j, :) = neighbors{frost};
-%         end
-%     end
 end
 
