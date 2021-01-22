8b55249ff67afd4a36d712e3210abff12746dbcb
Sam Feig <spfeig@gmail.com>
2021-01-21 17:48:39 -0700

    Frosty sorta not really working

--- H1/frosty.m
+++ H1/frosty.m
@@ -1,6 +1,35 @@
 function [ outImg ] = frosty( inImg, n, m )
-    % inImg(n, m) = 
+    [x, y, z] = size(inImg);
+    
+    outImg = zeros(x, y, z);
+    
+    for i = 1:x
+        for j = 1:y
+                neighbors = {};
+                for nn = -n/2:n/2
+                    for mm = -m/2:m/2
+                        idxx = i+nn;
+                        idxy = j+mm;
+                        if idxx < 1
+                            idxx = 1;
+                        elseif idxx > x
+                            idxx = x;
+                        end
+                        if idxy < 1
+                            idxy = 1;
+                        elseif idxy > y
+                            idxy = y;
+                        end
+                        % disp(inImg(idxx, idxy, :))
+                        neighbors = [neighbors, inImg(idxx, idxy, :)];
+                    end
+                end
 
+                frost = randi(numel(neighbors), 1);
+                % disp(neighbors{frost})
 
+                outImg(i, j, :) = neighbors{frost};
+        end
+    end
 end
 
