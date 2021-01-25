18b68710623f77263bb50ea2727fa1b9870798c6
Vladimir Zhdanov <vzhdanov99@gmail.com>
2021-01-25 12:40:28 -0700

    Some comments

--- H1/frosty.m
+++ H1/frosty.m
@@ -1,17 +1,25 @@
 function [ outImg ] = frosty( inImg, n, m )
     [x, y, z] = size(inImg);
     
+    % Instantiate output image
     outImg = uint8(zeros(x, y, z));
+    
+    % Calculate bidirectional offset for n and m
     n_offset = floor(n / 2);
     m_offset = floor(m / 2);
     
     for i = 1:x
+        % Left and right side of the m-window, bounded by the image size
         m_left = max([1 i - m_offset]);
         m_right = min([i + m_offset x]);
         for j = 1:y
+            % Left and right side of the n-window, bounded by the image
+            % size
             n_left = max([1 j - n_offset]);
             n_right = min([j + n_offset y]);
             
+            % Map pixel to output image, choosing random m and n within the
+            % specified windows
             outImg(i, j, :) = inImg(randi([m_left m_right]), randi([n_left n_right]), :);
         end
     end
--- H1/invert_L.m
+++ H1/invert_L.m
@@ -1,8 +1,11 @@
 function [ outImg ] = invert_L(inImg)
     outImg = inImg;
+    
+    % Loop through each value in the image
     for i = 1:size(inImg, 1)
         for j = 1:size(inImg, 2)
             for k = 1:size(inImg, 3)
+                % Invert each pixel value
                 outImg(i, j, k) = 255 - inImg(i, j, k);
             end
         end
