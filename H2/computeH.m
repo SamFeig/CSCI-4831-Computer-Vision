82e8cf22120ce172b0c357bddb220fac1af31ea3
Vladimir Zhdanov <vzhdanov99@gmail.com>
2021-02-05 19:19:10 -0700

    Finish warp1 function and add comments

--- H2/computeH.m
+++ H2/computeH.m
@@ -8,16 +8,20 @@
 % Instructor: Ioana Fleming
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function[ Hout, minDis ] = computeH(points, n)
-    H = zeros(3,3,20);
-    distance = zeros(20,1);
-    newPt = zeros(n,3);
+%% Task 2: Computing the Homography Parameters
+function[ H_out ] = computeH(points)
+    n = 20;
+    H = zeros(3, 3, n);
+    num_points = size(points, 1);
+    
+    distance = zeros(n, 1);
+    p = zeros(num_points,3);
 
     % Repeat 20 times
-    for i = 1:20
+    for i = 1:n
         % Select 4 random points
-        idx = randperm(n,4);
-        selectPoints = points(idx,:);
+        rand_indices = randperm(num_points,4);
+        selectPoints = points(rand_indices,:);
         
         A = zeros(8,9);
         for j = 1:4
@@ -26,27 +30,36 @@ function[ Hout, minDis ] = computeH(points, n)
             x2 = selectPoints(j,3);
             y2 = selectPoints(j,4);
             
-            % Solve linear equation
+            % Build A matrix from (x1, y1) and (x2, y2) values
             A(2*j-1, :) = [-x1, -y1, -1, 0, 0, 0, x1*x2, y1*x2, x2];
             A(2*j, :) = [0, 0, 0, -x1, -y1, -1, x1*y2, y1*y2, y2];
         end
     
-        % Svd of the 8*9 matrix
+        % SVD of the A matrix, we only need V in this case
         [~,~,V] = svd(A);
-        % Reshape last column of V into 3*3 matrix
-        % Compute Ax = b
-        H(:, :, i) = transpose(reshape(V(:, end), [3,3]));
         
-        % Calc projection coord
-        for j = 1:n
-          newPt(j, :) = H(:, :, j) * [points(j, 1); points(j, 2); 1];
-          newPt(j, :) = newPt(j, :) / newPt(j, 3);
+        % The last column of V is the solution for h.
+        h_sol = V(:, end);
+        
+        % Reshape V into H matrix based on formula in homography paper
+        H(:, :, i) = transpose(reshape(h_sol, [3,3]));
+        
+        % Project image 1 points onto image 2
+        for j = 1:num_points
+          p(j, :) = H(:, :, j) * [points(j, 1); points(j, 2); 1];
+          p(j, :) = p(j, :) / p(j, 3);
         end
-        % Calc euclidean distance for all 10 points
-        distance(i) = sum(sqrt(sum((newPt(:, 1:2) - points(:, 3:4)).^2, 2)));
+        
+        % Calculate Euclidean distance between all of the points in image 2
+        % and the transformed points from image 1
+        e_dist = sqrt(sum((points(:, 3) - p(:, 1)).^2 + (points(:, 4) - p(:, 2)).^2, 2));
+        
+        % Save the average distance value to find the minimum at the end
+        distance(i) = sum(e_dist) / num_points;
     end
-    % Find index of min distance
-    idx = find(distance == min(distance));
-    Hout = H(:, :, idx(1));
+    
+    % Find index of minimum average distance
+    min_index = find(distance == min(distance));
+    H_out = H(:, :, min_index(1));
 end
 
