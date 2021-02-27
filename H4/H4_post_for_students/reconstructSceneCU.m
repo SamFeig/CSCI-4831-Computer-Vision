f4138dc9c458eb7e4fc983133a44bfadf03db86f
Sam Feig <spfeig@gmail.com>
2021-02-27 15:48:10 -0700

    reconstructSceneCU working maybe?

--- H4/H4_post_for_students/reconstructSceneCU.m
+++ H4/H4_post_for_students/reconstructSceneCU.m
@@ -9,8 +9,14 @@
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [points3D] = reconstructSceneCU(disparityMap, stereoParams)
-    f = stereoParams.CameraParameters1.FocalLength
-    B = stereoParams.TranslationOfCamera2
-    points3D = f*B/disparityMap;
+    f = mean(stereoParams.CameraParameters1.FocalLength);
+    B = stereoParams.TranslationOfCamera2';
+    
+    for i=1:size(disparityMap, 1)
+        for j=1:size(disparityMap, 2)
+            points3D(i, j, :) = f*B./disparityMap(i, j);   
+        end
+    end
+    
 end
 
