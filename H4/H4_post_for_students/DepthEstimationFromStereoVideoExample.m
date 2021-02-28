1b6e467eead399f6cb6a178b735c83ed69b4c4a3
Sam Feig <spfeig@gmail.com>
2021-02-28 16:46:53 -0700

    Create functions. Q4, Q5 still not working. Generate figures

--- H4/H4_post_for_students/DepthEstimationFromStereoVideoExample.m
+++ H4/H4_post_for_students/DepthEstimationFromStereoVideoExample.m
@@ -5,6 +5,9 @@
 % Load the |stereoParameters| object, which is the result of calibrating the 
 % camera using either the |stereoCameraCalibrator| app or the |estimateCameraParameters| 
 % function.
+close all;
+clear all;
+clc;
 
 % Load the stereoParameters object.
 load('handshakeStereoParams.mat');
@@ -47,137 +50,222 @@ title('Rectified Video Frames');
 frameLeftGray  = rgb2gray(frameLeftRect);
 frameRightGray = rgb2gray(frameRightRect);
     
-% disparityMap = disparity(frameLeftGray, frameRightGray);
-disparityMap = disparitySSD(frameLeftGray, frameRightGray, 3);
+disparityMap = disparity(frameLeftGray, frameRightGray);
+disparityMapRL = disparity(frameRightGray, frameLeftGray);
 
-figure;
-imshow(disparityMap, [0, 64]);
-title('Disparity Map');
+% disparityMap1 = disparitySSD(frameLeftGray, frameRightGray, 1);
+disparityMap3 = disparitySSD(frameLeftGray, frameRightGray, 3);
+disparityMap5 = disparitySSD(frameLeftGray, frameRightGray, 5);
+disparityMap3RL = disparitySSD(frameRightGray, frameLeftGray, 3);
+disparityMap5RL = disparitySSD(frameRightGray, frameLeftGray, 5);
+
+% % Question 1
+% figure
+% subplot(2, 2, 1)
+% imshow(disparityMap, [0, 64]);
+% title('Disparity Map Matlab') 
+% colormap jet
+% colorbar
+% subplot(2, 2, 2)
+% imshow(disparityMap1, [0, 64]);
+% title('Disparity SSD Window Size 1')
+% colormap jet
+% colorbar
+% subplot(2, 2, 3)
+% imshow(disparityMap3, [0, 64]);
+% title('Disparity SSD Window Size 3')
+% colormap jet
+% colorbar
+% subplot(2, 2, 4)
+% imshow(disparityMap5, [0, 64]);
+% title('Disparity SSD Window Size 5')
+% colormap jet
+% colorbar
+% saveas(gcf,'Q1_disparityMap.jpg')
+
+% Question 4 
+
+% Matlab reference
+% figure
+% subplot(1, 2, 1)
+% imshow(disparityMap, [0, 64]);
+% title('Matlab LR')
+% colormap jet
+% colorbar
+% subplot(1, 2, 2)
+% imshow(disparityMapRL, [0, 64]);
+% title('Matlab RL') 
+% colormap jet
+% colorbar
+% 
+% figure
+% imshow(detectOutliers(disparityMap, disparityMapRL, 1));
+% title('Outliers Matlab')
+
+% Question 4
+figure
+subplot(2, 2, 1)
+imshow(disparityMap3, [0, 64]);
+title('SSD Window Size 3 LR')
 colormap jet
 colorbar
-%% Reconstruct the 3-D Scene
-% Reconstruct the 3-D world coordinates of points corresponding to each pixel 
-% from the disparity map.
+subplot(2, 2, 2)
+imshow(disparityMap3RL, [0, 64]);
+title('SSD Window Size 3 RL') 
+colormap jet
+colorbar
+subplot(2, 2, 3)
+imshow(disparityMap5, [0, 64]);
+title('SSD Window Size 5 LR')
+colormap jet
+colorbar
+subplot(2, 2, 4)
+imshow(disparityMap5RL, [0, 64]);
+title('SSD Window Size RL') 
+colormap jet
+colorbar
 
-points3D = reconstructScene(disparityMap, stereoParams);
+figure
+subplot(1, 2, 1)
+imshow(detectOutliers(disparityMap3, disparityMap3RL, 1));
+title('Outliers SSD Window Size 3')
+subplot(1, 2, 2)
+imshow(detectOutliers(disparityMap5, disparityMap5RL, 1));
+title('Outliers SSD Window Size 5') 
+colormap gray
+saveas(gcf,'Q4_OutliersMap.jpg')
+
+% figure;
+% imshow(disparityMap, [0, 64]);
+% title('Disparity Map');
+% colormap jet
+% colorbar
+
+
+% %% Reconstruct the 3-D Scene
+% % Reconstruct the 3-D world coordinates of points corresponding to each pixel 
+% % from the disparity map.
+% 
+% % points3D = reconstructScene(disparityMap, stereoParams);
 % points3D = reconstructSceneCU(disparityMap, stereoParams);
-
-% Convert to meters and create a pointCloud object
-points3D = points3D ./ 1000;
-ptCloud = pointCloud(points3D, 'Color', frameLeftRect);
-
-% Create a streaming point cloud viewer
-player3D = pcplayer([-3, 3], [-3, 3], [0, 8], 'VerticalAxis', 'y', ...
-    'VerticalAxisDir', 'down');
-
-% Visualize the point cloud
-view(player3D, ptCloud);
-%% Detect People in the Left Image
-% Use the |vision.PeopleDetector| system object to detect people.
-
-% Create the people detector object. Limit the minimum object size for
-% speed.
-peopleDetector = vision.PeopleDetector('MinSize', [166 83]);
-
-% Detect people.
-bboxes = peopleDetector.step(frameLeftGray);
-%% Determine The Distance of Each Person to the Camera
-% Find the 3-D world coordinates of the centroid of each detected person and 
-% compute the distance from the centroid to the camera in meters.
-
-% Find the centroids of detected people.
-centroids = [round(bboxes(:, 1) + bboxes(:, 3) / 2), ...
-    round(bboxes(:, 2) + bboxes(:, 4) / 2)];
-
-% Find the 3-D world coordinates of the centroids.
-centroidsIdx = sub2ind(size(disparityMap), centroids(:, 2), centroids(:, 1));
-X = points3D(:, :, 1);
-Y = points3D(:, :, 2);
-Z = points3D(:, :, 3);
-centroids3D = [X(centroidsIdx)'; Y(centroidsIdx)'; Z(centroidsIdx)'];
-
-% Find the distances from the camera in meters.
-dists = sqrt(sum(centroids3D .^ 2));
-    
-% Display the detected people and their distances.
-labels = cell(1, numel(dists));
-for i = 1:numel(dists)
-    labels{i} = sprintf('%0.2f meters', dists(i));
-end
-figure;
-imshow(insertObjectAnnotation(frameLeftRect, 'rectangle', bboxes, labels));
-title('Detected People');
-%% Process the Rest of the Video
-% Apply the steps described above to detect people and measure their distances 
-% to the camera in every frame of the video.
-
-while ~isDone(readerLeft) && ~isDone(readerRight)
-    % Read the frames.
-    frameLeft = readerLeft.step();
-    frameRight = readerRight.step();
-    
-    % Rectify the frames.
-    [frameLeftRect, frameRightRect] = ...
-        rectifyStereoImages(frameLeft, frameRight, stereoParams);
-    
-    % Convert to grayscale.
-    frameLeftGray  = rgb2gray(frameLeftRect);
-    frameRightGray = rgb2gray(frameRightRect);
-    
-    % Compute disparity. 
-    disparityMap = disparity(frameLeftGray, frameRightGray);
-    
-    % Reconstruct 3-D scene.
-    points3D = reconstructScene(disparityMap, stereoParams);
-    points3D = points3D ./ 1000;
-    ptCloud = pointCloud(points3D, 'Color', frameLeftRect);
-    view(player3D, ptCloud);
-    
-    % Detect people.
-    bboxes = peopleDetector.step(frameLeftGray);
-    
-    if ~isempty(bboxes)
-        % Find the centroids of detected people.
-        centroids = [round(bboxes(:, 1) + bboxes(:, 3) / 2), ...
-            round(bboxes(:, 2) + bboxes(:, 4) / 2)];
-        
-        % Find the 3-D world coordinates of the centroids.
-        centroidsIdx = sub2ind(size(disparityMap), centroids(:, 2), centroids(:, 1));
-        X = points3D(:, :, 1);
-        Y = points3D(:, :, 2);
-        Z = points3D(:, :, 3);
-        centroids3D = [X(centroidsIdx), Y(centroidsIdx), Z(centroidsIdx)];
-        
-        % Find the distances from the camera in meters.
-        dists = sqrt(sum(centroids3D .^ 2, 2));
-        
-        % Display the detect people and their distances.
-        labels = cell(1, numel(dists));
-        for i = 1:numel(dists)
-            labels{i} = sprintf('%0.2f meters', dists(i));
-        end
-        dispFrame = insertObjectAnnotation(frameLeftRect, 'rectangle', bboxes,...
-            labels);
-    else
-        dispFrame = frameLeftRect;
-    end
-    
-    % Display the frame.
-    step(player, dispFrame);
-end
-
-% Clean up.
-reset(readerLeft);
-reset(readerRight);
-release(player);
-%% Summary
-% This example showed how to localize pedestrians in 3-D using a calibrated 
-% stereo camera.
-%% References
-% [1] G. Bradski and A. Kaehler, "Learning OpenCV : Computer Vision with the 
-% OpenCV Library," O'Reilly, Sebastopol, CA, 2008.
 % 
-% [2] Dalal, N. and Triggs, B., Histograms of Oriented Gradients for Human 
-% Detection. CVPR 2005.
+% % Convert to meters and create a pointCloud object
+% points3D = points3D ./ 1000;
+% ptCloud = pointCloud(points3D, 'Color', frameLeftRect);
 % 
-% _Copyright 2013-2014 The MathWorks, Inc._
+% % Create a streaming point cloud viewer
+% player3D = pcplayer([-3, 3], [-3, 3], [0, 8], 'VerticalAxis', 'y', ...
+%     'VerticalAxisDir', 'down');
+% 
+% % Visualize the point cloud
+% view(player3D, ptCloud);
+% %% Detect People in the Left Image
+% % Use the |vision.PeopleDetector| system object to detect people.
+% 
+% % Create the people detector object. Limit the minimum object size for
+% % speed.
+% peopleDetector = vision.PeopleDetector('MinSize', [166 83]);
+% 
+% % Detect people.
+% bboxes = peopleDetector.step(frameLeftGray);
+% %% Determine The Distance of Each Person to the Camera
+% % Find the 3-D world coordinates of the centroid of each detected person and 
+% % compute the distance from the centroid to the camera in meters.
+% 
+% % Find the centroids of detected people.
+% centroids = [round(bboxes(:, 1) + bboxes(:, 3) / 2), ...
+%     round(bboxes(:, 2) + bboxes(:, 4) / 2)];
+% 
+% % Find the 3-D world coordinates of the centroids.
+% centroidsIdx = sub2ind(size(disparityMap), centroids(:, 2), centroids(:, 1));
+% X = points3D(:, :, 1);
+% Y = points3D(:, :, 2);
+% Z = points3D(:, :, 3);
+% centroids3D = [X(centroidsIdx)'; Y(centroidsIdx)'; Z(centroidsIdx)'];
+% 
+% % Find the distances from the camera in meters.
+% dists = sqrt(sum(centroids3D .^ 2));
+%     
+% % Display the detected people and their distances.
+% labels = cell(1, numel(dists));
+% for i = 1:numel(dists)
+%     labels{i} = sprintf('%0.2f meters', dists(i));
+% end
+% figure;
+% imshow(insertObjectAnnotation(frameLeftRect, 'rectangle', bboxes, labels));
+% title('Detected People');
+% %% Process the Rest of the Video
+% % Apply the steps described above to detect people and measure their distances 
+% % to the camera in every frame of the video.
+% 
+% while ~isDone(readerLeft) && ~isDone(readerRight)
+%     % Read the frames.
+%     frameLeft = readerLeft.step();
+%     frameRight = readerRight.step();
+%     
+%     % Rectify the frames.
+%     [frameLeftRect, frameRightRect] = ...
+%         rectifyStereoImages(frameLeft, frameRight, stereoParams);
+%     
+%     % Convert to grayscale.
+%     frameLeftGray  = rgb2gray(frameLeftRect);
+%     frameRightGray = rgb2gray(frameRightRect);
+%     
+%     % Compute disparity. 
+%     disparityMap = disparity(frameLeftGray, frameRightGray);
+%     
+%     % Reconstruct 3-D scene.
+% %     points3D = reconstructScene(disparityMap, stereoParams);
+%     points3D = reconstructSceneCU(disparityMap, stereoParams);
+%     points3D = points3D ./ 1000;
+%     ptCloud = pointCloud(points3D, 'Color', frameLeftRect);
+%     view(player3D, ptCloud);
+%     
+%     % Detect people.
+%     bboxes = peopleDetector.step(frameLeftGray);
+%     
+%     if ~isempty(bboxes)
+%         % Find the centroids of detected people.
+%         centroids = [round(bboxes(:, 1) + bboxes(:, 3) / 2), ...
+%             round(bboxes(:, 2) + bboxes(:, 4) / 2)];
+%         
+%         % Find the 3-D world coordinates of the centroids.
+%         centroidsIdx = sub2ind(size(disparityMap), centroids(:, 2), centroids(:, 1));
+%         X = points3D(:, :, 1);
+%         Y = points3D(:, :, 2);
+%         Z = points3D(:, :, 3);
+%         centroids3D = [X(centroidsIdx), Y(centroidsIdx), Z(centroidsIdx)];
+%         
+%         % Find the distances from the camera in meters.
+%         dists = sqrt(sum(centroids3D .^ 2, 2));
+%         
+%         % Display the detect people and their distances.
+%         labels = cell(1, numel(dists));
+%         for i = 1:numel(dists)
+%             labels{i} = sprintf('%0.2f meters', dists(i));
+%         end
+%         dispFrame = insertObjectAnnotation(frameLeftRect, 'rectangle', bboxes,...
+%             labels);
+%     else
+%         dispFrame = frameLeftRect;
+%     end
+%     
+%     % Display the frame.
+%     step(player, dispFrame);
+% end
+% 
+% % Clean up.
+% reset(readerLeft);
+% reset(readerRight);
+% release(player);
+% %% Summary
+% % This example showed how to localize pedestrians in 3-D using a calibrated 
+% % stereo camera.
+% %% References
+% % [1] G. Bradski and A. Kaehler, "Learning OpenCV : Computer Vision with the 
+% % OpenCV Library," O'Reilly, Sebastopol, CA, 2008.
+% % 
+% % [2] Dalal, N. and Triggs, B., Histograms of Oriented Gradients for Human 
+% % Detection. CVPR 2005.
+% % 
+% % _Copyright 2013-2014 The MathWorks, Inc._
