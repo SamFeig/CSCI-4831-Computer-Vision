a201360528e187de309b22176b410a1218cb4d87
Vladimir Zhdanov <vzhdanov99@gmail.com>
2021-03-17 01:11:00 -0600

    Task 5 kinda done I guess? Idk what else to add

--- H5/code/ComputeFeatures.m
+++ H5/code/ComputeFeatures.m
@@ -14,10 +14,25 @@ function features = ComputeFeatures(img)
     img = double(img);
     height = size(img, 1);
     width = size(img, 2);
-    features = zeros([height, width, 1]);
+    features = zeros(height, width, 7);
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %                                                                         %
     %                              YOUR CODE HERE                             %
     %                                                                         %
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % First three positions are the RGB values of the image
+    features(:, :, 1:3) = double(img);
+    
+    % 4th position is the x-value of the image
+    features(:, :, 4) = meshgrid(1:width, 1:height);
+    
+    % 5th position is the y-value of the image
+    features(:, :, 5) = meshgrid(1:height, 1:width)';
+    
+    % 6th position is binary value if pixel is at an edge
+    features(:, :, 6) = edge(rgb2gray(img));
+    
+    % 7th and 8th positions are x & y directional gradients
+    [features(:, :, 7), features(:, :, 8)] = imgradient(rgb2gray(img));
+    
 end
