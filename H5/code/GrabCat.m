992ec8195468539aeacf27e86f4e2e0005f59b31
Vladimir Zhdanov <vzhdanov99@gmail.com>
2021-03-18 16:21:25 -0600

    Task 7 done

---
+++ H5/code/GrabCat.m
@@ -0,0 +1,47 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Sam Feig 
+% Vladimir Zhdanov
+%
+% CSCI 4831/5722
+% Homework 5
+% Instructor: Ioana Fleming
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Task 7 - GrabCat: Transfer Segments between Images
+% GrabCat.m starter code wasn't provided, but most of this code is from
+% `RunComputeSegmentation.m`
+
+% Read the input image and background image.
+img = imread('../imgs/black_kitten_star.jpg');
+bg = imread('../imgs/backgrounds/desert.jpg');
+
+% Choose the number of clusters and the clustering method.
+k = 3;
+clusteringMethod = 'kmeans';
+
+% Choose the feature function that will be used. The @ syntax creates a
+% function handle; this allows us to pass a function as an argument to
+% another function.
+featureFn = @ComputeColorFeatures;
+
+% Whether or not to normalize features before clustering.
+normalizeFeatures = true;
+
+% Whether or not to resize the image before clustering. If this script
+% runs too slowly then you should set resize to a value less than 1.
+resize = .2;
+
+% Use all of the above parameters to actually compute a segmentation.
+segments = ComputeSegmentation(img, k, clusteringMethod, featureFn, ...
+                               normalizeFeatures, resize);
+           
+% Use ChooseSegments to move chosen segments to the background
+[mask, simg] = ChooseSegments(segments, bg);
+
+% Show the composite image
+figure
+imshow(simg)
+
+% Save the final image
+saveas(gcf,'grabCatOutput.jpg')
